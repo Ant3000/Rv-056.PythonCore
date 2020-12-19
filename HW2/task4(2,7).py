3b036424c107566b4e6644ddad755dbeee61457b
ReSpekTeR <respekter.ua@gmail.com>
2020-12-19 20:25:03 +0200

    Added new files

---
+++ HW2/task4(2,7).py
@@ -0,0 +1,31 @@
+#Python 2.x 
+a =  int(raw_input("Enter a = "))
+b =  int(raw_input("Enter b = "))
+print "a + b =", a+b
+print "a - b =", a-b
+print "a * b =", a*b
+print "a / b =", float(a/b) #bug
+
+q1=raw_input("What is your name? ")
+q2=raw_input("How old are you? ")
+q3=raw_input("Where do you live? ")
+print "Hello", q1
+print "Your age is", q2
+print "You live in", q3
+
+print "            ------           	 "   
+print "        --          --     		 "
+print "      -                -          "
+print "    --    **      **    --        "   
+print "   -     ****    ****     -       "
+print "  -       **      **       -      "
+print " -                          -     "
+print " -                           -    "
+print " -                           -    "
+print " -     **               **   -    "
+print "  -      **           **     -    "
+print "   -       **      **       -     "
+print "    --       *****       --       "
+print "      -                -          "
+print "        --          --            "
+print "           --------               "  

b3afc24655e2adad025073f3ffd068fd7abc23fb
ReSpekTeR <respekter.ua@gmail.com>
2020-12-19 22:38:43 +0200

    changes according to recommendations

--- HW2/task2.py
+++ HW2/task2.py
@@ -1,6 +1,6 @@
-q1 = input('What is your name? ')
-q2 = input('How old are you? ')
-q3 = input('Where do you live? ')
+name = input('What is your name? ')
+age = input('How old are you? ')
+city = input('Where do you live? ')
 
-print ('Hello '+q1+'. Your age is '+q2+'. You live in '+q3)
-# print ('Hello {0}. Your age is {1}. You live in {2}'.format(q1,q2,q3))
+print ('Hello '+name+'. Your age is '+age+'. You live in '+city)
+# print ('Hello {0}. Your age is {1}. You live in {2}'.format(name,age,city))

5fe80b6df984425dce975001865916aa3f19ff4b
Oleksandr Tsapin <otsapin@gmail.com>
2020-12-21 17:45:48 +0200

    Remove files

--- tsapin/task4/task2_py27.py
+++
@@ -1,9 +0,0 @@
-# Task 2
-# Python 2.7
-
-name = raw_input("What is your name? ")
-age = raw_input("How old are you? ")
-city = raw_input("Where do you live? ")
-print "Hello ", name
-print "Your age is ", age
-print "You live in ", city

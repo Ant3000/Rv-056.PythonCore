013951da1c27d8f57782e77a0ea51809c883f1b0
ViktorPakhai <sdgadmin@gmail.com>
2020-12-21 15:02:31 +0200

    Add files from HomeWork 2

---
+++ pakhai/HW2/Task03.py
@@ -0,0 +1,12 @@
+# Home work lection_01
+
+print (" ************ ")
+print ("*  **   **   *")
+print ("*  **   **   *")
+print ("*            *")
+print ("*            *")
+print ("*  *      *  *")
+print ("*   ******   *")
+print (" ************ ")
+print ("              ")
+print ("              ")

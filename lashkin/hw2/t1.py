6d2b8c52d1e30ebde3fee7ca8e8b4ac62bb1996e
lalalash <64227259+lalalash@users.noreply.github.com>
2020-12-22 00:19:00 +0100

    1st commit

---
+++ lashkin/hw2/t1.py
@@ -0,0 +1,15 @@
+#Task 1
+#Define variables a and b. Read values a and b from console and calculate:
+#a + b
+#a - b
+#a * b
+#a / b.
+#   Output obtained results.
+
+a = int(input("what's your a? "))
+b = int(input("what's your b? "))
+
+print(' a + b = ', a+b)
+print(' a - b = ', a-b)
+print(' a * b = ', a*b)
+print(' a / b = ', a/b)

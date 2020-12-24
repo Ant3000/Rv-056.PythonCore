1b740e12cac3027e0bdb85f7d3e0c726a6624ef7
katemalash <35248902+katemalash@users.noreply.github.com>
2020-12-24 20:16:38 +0200

    Correction

--- naydaN/HW2/work2.py
+++
@@ -1,4 +0,0 @@
-first_name=input("what is your name?")
-age=input(" how old are you?")
-city=input("Where do you live?")
-print("Hello!" + " " "My name is " + first_name + ",", "I am " + age + ",", "I live in " + city +".")

d456f12af9873c78e7095b376f89eb4e2ab8f581
Ihor_Svietlov <igaross3590@gmail.com>
2020-12-26 20:21:20 +0200

    added first task for hw3

---
+++ Svietlov/homework3/hw_no_temp.py
@@ -0,0 +1,10 @@
+print("number switcher")
+a = int(input("enter you first number"))
+print("first number is", a)
+b = int(input("enter your second number"))
+print("second number is", b)
+a = -a - b
+b = -a - b
+a = -a - b
+print("a little magic")
+print("now first number is", a, "and second number is", b)

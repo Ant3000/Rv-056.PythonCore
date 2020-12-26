f5ec48f2bf99efa23607eae05dc26c5a72fd9786
Ihor_Svietlov <igaross3590@gmail.com>
2020-12-26 22:11:10 +0200

    added third task for hw3

---
+++ Svietlov/homework3/perimetr_of_retangle.py
@@ -0,0 +1,4 @@
+width_retangle = int(input("enter width in cm "))
+height_retangle = int(input("enter height in cm "))
+perimetr_retangle = 2 * (width_retangle + height_retangle)
+print("perimetr of retangle is", perimetr_retangle, "cm")

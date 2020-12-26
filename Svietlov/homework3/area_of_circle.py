140b5765785f6d01f866c1063ab8e7ca98c65aa4
Ihor_Svietlov <igaross3590@gmail.com>
2020-12-26 22:41:22 +0200

    added the fourth task for hw3

---
+++ Svietlov/homework3/area_of_circle.py
@@ -0,0 +1,4 @@
+import math
+rad = int(input("radius of the circle in cm "))
+print("area of the circle is", math.pi * (rad ** 2), "cm")
+print("perimeter of the circle is ", 2 * math.pi * rad, "cm")

cee56305b83d5eb78ac2809b70b9d353bb8e58fa
Ihor_Svietlov <igaross3590@gmail.com>
2020-12-26 22:58:57 +0200

    added the fifth task for hw3

---
+++ Svietlov/homework3/hypotenuse_of_triangle.py
@@ -0,0 +1,4 @@
+import math
+first_side = float(input("enter the first side of triangle "))
+second_side = float(input("enter the second side of triangle "))
+print("hypotenuse is ", math.sqrt (first_side ** 2 + second_side ** 2))

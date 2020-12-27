947f426e6584c5d4bd4893317b6fe372d291ccd0
Nathalie Nayda <nathalie.nayda@gmail.com>
2020-12-27 18:32:19 +0200

    HW3 added

--- naydaN/HW3/HW3-4.py
+++ naydaN/HW3/HW3-4.py
@@ -1,3 +1,4 @@
 import math
 radius = int(input ("радіус круга= "))
-print('Площа круга дорівнює:', math.pi * (radius ** 2))
+print('Площа круга дорівнює:', math.pi * radius**2)
+print('Довжина круга дорівнює:', 2 * math.pi * radius)

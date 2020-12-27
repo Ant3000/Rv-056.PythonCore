d9fdb018fcc37f09cda85ef90fc1600948d5ecac
ant7744 <ancientrex@gmail.com>
2020-12-27 19:32:28 +0200

    123

---
+++ GOshko/hw2/GOshkoHW1task3.py
@@ -0,0 +1,13 @@
+#task 1
+a=int(input('enter a = '))
+b=int(input('enter b = '))
+print('a+b =', a + b, "\na-b =", a - b, '\na*b =', a * b, '\na/b =', a / b)
+
+#task 2
+name = input('What is your name? ')
+age = input('How old are you? ')
+city = input('Where do you live? ')
+print('Hello', name, '\nYour age is', age, '\nYou live in', city)
+
+#task3
+print('(◕‿◕)')
